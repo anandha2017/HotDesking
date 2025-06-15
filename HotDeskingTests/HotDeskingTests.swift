feda96ee8f599f91dfbcfedc6162527d8acd1696
anandha2017 <github2017@ponnampalam.ca>
2025-06-15 21:45:58 +0100

    Initial Commit

---
+++ HotDeskingTests/HotDeskingTests.swift
@@ -0,0 +1,17 @@
+//
+//  HotDeskingTests.swift
+//  HotDeskingTests
+//
+//  Created by Anandha Ponnampalam on 15/06/2025.
+//
+
+import Testing
+@testable import HotDesking
+
+struct HotDeskingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
